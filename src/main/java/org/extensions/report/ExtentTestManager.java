a1f16019a8544731f47c76f97598d9e7036f941d
aviadamd <aviadamd@gmail.com>
2023-04-04 21:27:53 +0300

    fix

--- src/main/java/org/extensions/report/ExtentTestManager.java
+++ src/main/java/org/extensions/report/ExtentTestManager.java
@@ -15,27 +15,33 @@ import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Date;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Slf4j
 public class ExtentTestManager {
     private static final ThreadLocal<ExtentTest> extentTest = new ThreadLocal<>();
     protected static ExtentReports getReportsInstance = ExtentManager.getReportsInstance();
-    protected static ExtentSparkReporter getSparkReporterInstance() { return ExtentManager.getExtentSparkInstance(); }
     protected synchronized static ExtentTest getExtentTest() { return extentTest.get(); }
-    protected synchronized static ExtentTest createTest(String testMethod, String category, String author) {
-        ExtentTest test = getReportsInstance
-                .createTest(testMethod)
+
+    /**
+     * createTest
+     * @param testMethod
+     * @param category
+     * @param author
+     */
+    protected synchronized static void createTest(String testMethod, String category, String author) {
+        extentTest.set(getReportsInstance.createTest(testMethod)
                 .createNode(testMethod)
                 .assignCategory(category)
-                .assignAuthor(author);
-        extentTest.set(test);
-        return getExtentTest();
+                .assignAuthor(author)
+        );
     }
+
+    /**
+     * attachExtraReports
+     * @param extraReportsBy
+     * @param path
+     */
     protected synchronized static void attachExtraReports(Status[] extraReportsBy, String path) {
         for (Status status : extraReportsBy) {
             String reportPath = path + "/" + status.toString() + ".html";
@@ -44,62 +50,125 @@ public class ExtentTestManager {
         }
     }
 
+    /**
+     * getScreenShot
+     * @param driver
+     * @param screenshotName
+     * @return
+     */
     public synchronized static String getScreenShot(WebDriver driver, String screenshotName) {
         String destination = "";
         try {
-            String dateName = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
-            TakesScreenshot ts = (TakesScreenshot) driver;
-            File source = ts.getScreenshotAs(OutputType.FILE);
             String fileDirPath = System.getProperty("user.dir") + "/target/tests_screen_shots";
             createDir(Path.of(fileDirPath));
-            destination = fileDirPath + "/" + screenshotName + dateName + ".png";
+            destination = fileDirPath + "/" + screenshotName + new SimpleDateFormat("yyyyMMddhhmmss").format(new Date()) + ".png";
             File finalDestination = new File(destination);
-            FileUtils.copyFile(source, finalDestination);
-        } catch (Exception ignore) {}
-
-        return destination;
+            FileUtils.copyFile(((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE), finalDestination);
+            return destination;
+        } catch (Exception ignore) {
+            return destination;
+        }
     }
+
+    /**
+     * assignCategory
+     * @param assignCategory
+     */
     public synchronized static void assignCategory(String assignCategory) {
         extentTest.get().assignCategory(assignCategory);
     }
 
+    /**
+     * log
+     * @param status
+     * @param media
+     */
     public synchronized static void log(Status status, Media media) {
-        getExtentTest().log(status, media);
+        extentTest.get().log(status, media);
         log.info(status + " " + media.getTitle());
     }
 
+    /**
+     * log
+     * @param status
+     * @param details
+     */
     public synchronized static void log(Status status, String details) {
-        getExtentTest().log(status, details);
+        extentTest.get().log(status, details);
         log.info(status + " " + details);
     }
+
+    /**
+     * log
+     * @param status
+     * @param markup
+     */
     public synchronized static void log(Status status, Markup markup) {
-        getExtentTest().log(status, markup);
+        extentTest.get().log(status, markup);
         log.info(status + " " + markup.getMarkup());
     }
+
+    /**
+     * log
+     * @param status
+     * @param throwable
+     */
     public synchronized static void log(Status status, Throwable throwable) {
-        getExtentTest().log(status, throwable);
+        extentTest.get().log(status, throwable);
         log.info(status + " " + throwable.getMessage());
     }
+
+    /**
+     * log
+     * @param status
+     * @param details
+     * @param media
+     */
     public synchronized static void log(Status status, String details, Media media) {
-        getExtentTest().log(status, details, media);
+        extentTest.get().log(status, details, media);
         log.info(status + " " + details);
     }
+
+    /**
+     * log
+     * @param status
+     * @param throwable
+     * @param media
+     */
     public synchronized static void log(Status status, Throwable throwable, Media media) {
-        getExtentTest().log(status, throwable, media);
+        extentTest.get().log(status, throwable, media);
         log.info(status + " " + throwable.getMessage());
     }
+
+    /**
+     * log
+     * @param status
+     * @param details
+     * @param throwable
+     * @param media
+     */
     public synchronized static void log(Status status, String details, Throwable throwable, Media media) {
-        getExtentTest().log(status, details, throwable, media);
+        extentTest.get().log(status, details, throwable, media);
         log.info(status + " " +  details + " " + throwable.getMessage());
     }
 
+    /**
+     * logScreenShot
+     * @param status
+     * @param driver
+     * @param message
+     */
     public synchronized static void logScreenShot(Status status, WebDriver driver, String message) {
-        if (getExtentTest() != null) {
-            log(status, ExtentTestManager.getScreenShot(driver, message));
-            log.info(status + " " + message);
+        if (extentTest.get() != null && driver != null) {
+            extentTest.get().log(status, ExtentTestManager.getScreenShot(driver, message));
+            log.info(status + " " +  message);
         }
     }
 
+    /**
+     * createDir
+     * @param path
+     */
     private synchronized static void createDir(Path path) {
         try {
             Files.createDirectory(path);
