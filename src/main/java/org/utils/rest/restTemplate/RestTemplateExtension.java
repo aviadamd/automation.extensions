77f8895fba1d230a67dbeced0bd2ff4bfc1739ae
aviadamd <aviadamd@gmail.com>
2023-05-09 06:31:52 +0300

    fix

--- src/main/java/org/utils/rest/restTemplate/RestTemplateExtension.java
+++ src/main/java/org/utils/rest/restTemplate/RestTemplateExtension.java
@@ -22,7 +22,7 @@ public class RestTemplateExtension {
     private final RestTemplate restTemplate;
 
     public RestTemplateExtension() {
-        this.restTemplate = new RestTemplate(this.clientHttpRequestFactory());
+        this.restTemplate = new RestTemplate(this.httpRequestFactory(null));
     }
 
     public void setClientHttpRequestFactory(ClientHttpRequestFactory clientHttpRequestFactory) {
@@ -33,98 +33,104 @@ public class RestTemplateExtension {
         return UriComponentsBuilder.newInstance().scheme(scheme).host(host);
     }
 
-    public <T> RestTemplateCollector<T> get(UriComponentsBuilder uriBuilder, Class<T> responseT) {
-        RestTemplateCollector<T> restTemplateCollector = new RestTemplateCollector<>();
+    public <T> ResponseAdapter<T> get(UriComponentsBuilder uriBuilder, Class<T> responseT) {
+        ResponseAdapter<T> responseAdapter = new ResponseAdapter<>();
         try {
-            restTemplateCollector.setResponseEntity(this.restTemplate.getForEntity(uriBuilder.toUriString(), responseT));
-            restTemplateCollector.setPassResponse(true);
+            final String uri = uriBuilder.toUriString();
+            responseAdapter.setResponse(this.restTemplate.getForEntity(uri, responseT));
+            responseAdapter.setPassResponse(true);
         } catch (RestClientResponseException restClientResponseException) {
-            restTemplateCollector.setPassResponse(false);
-            restTemplateCollector.setRestClientResponseException(restClientResponseException);
+            responseAdapter.setPassResponse(false);
+            responseAdapter.setResponseException(restClientResponseException);
         } catch (Exception exception) {
-            restTemplateCollector.setPassResponse(false);
-            restTemplateCollector.setGeneralException(exception);
+            responseAdapter.setPassResponse(false);
+            responseAdapter.setGeneralException(exception);
         }
-        return restTemplateCollector;
+        return responseAdapter;
     }
 
-    public <T> RestTemplateCollector<T> get(UriComponentsBuilder uriBuilder, HttpEntity<?> httpEntity, Class<T> responseT) {
-        RestTemplateCollector<T> restTemplateCollector = new RestTemplateCollector<>();
+    public <T> ResponseAdapter<T> get(UriComponentsBuilder uriBuilder, HttpEntity<?> httpEntity, Class<T> responseT) {
+        ResponseAdapter<T> responseAdapter = new ResponseAdapter<>();
         try {
-            restTemplateCollector.setResponseEntity(this.restTemplate.exchange(uriBuilder.toUriString(), HttpMethod.GET, httpEntity, responseT));
-            restTemplateCollector.setPassResponse(true);
+            final String uri = uriBuilder.toUriString();
+            responseAdapter.setResponse(this.restTemplate.exchange(uri, HttpMethod.GET, httpEntity, responseT));
+            responseAdapter.setPassResponse(true);
         } catch (RestClientResponseException restClientResponseException) {
-            restTemplateCollector.setPassResponse(false);
-            restTemplateCollector.setRestClientResponseException(restClientResponseException);
+            responseAdapter.setPassResponse(false);
+            responseAdapter.setResponseException(restClientResponseException);
         } catch (Exception exception) {
-            restTemplateCollector.setPassResponse(false);
-            restTemplateCollector.setGeneralException(exception);
+            responseAdapter.setPassResponse(false);
+            responseAdapter.setGeneralException(exception);
         }
-        return restTemplateCollector;
+        return responseAdapter;
     }
 
-    public <T> RestTemplateCollector<T> put(UriComponentsBuilder uriBuilder, HttpEntity<?> httpEntity, Class<T> responseT) {
-        RestTemplateCollector<T> restTemplateCollector = new RestTemplateCollector<>();
+    public <T> ResponseAdapter<T> put(UriComponentsBuilder uriBuilder, HttpEntity<?> httpEntity, Class<T> responseT) {
+        ResponseAdapter<T> responseAdapter = new ResponseAdapter<>();
         try {
-            restTemplateCollector.setResponseEntity(this.restTemplate.exchange(uriBuilder.toUriString(), HttpMethod.PUT, httpEntity, responseT));
-            restTemplateCollector.setPassResponse(true);
+            final String uri = uriBuilder.toUriString();
+            responseAdapter.setResponse(this.restTemplate.exchange(uri, HttpMethod.PUT, httpEntity, responseT));
+            responseAdapter.setPassResponse(true);
         } catch (RestClientResponseException restClientResponseException) {
-            restTemplateCollector.setPassResponse(false);
-            restTemplateCollector.setRestClientResponseException(restClientResponseException);
+            responseAdapter.setPassResponse(false);
+            responseAdapter.setResponseException(restClientResponseException);
         } catch (Exception exception) {
-            restTemplateCollector.setPassResponse(false);
-            restTemplateCollector.setGeneralException(exception);
+            responseAdapter.setPassResponse(false);
+            responseAdapter.setGeneralException(exception);
         }
-        return restTemplateCollector;
+        return responseAdapter;
     }
 
-    public <T> RestTemplateCollector<T> post(UriComponentsBuilder uriBuilder, HttpEntity<?> httpEntity, Class<T> responseT) {
-        RestTemplateCollector<T> restTemplateCollector = new RestTemplateCollector<>();
+    public <T> ResponseAdapter<T> post(UriComponentsBuilder uriBuilder, HttpEntity<?> httpEntity, Class<T> responseT) {
+        ResponseAdapter<T> responseAdapter = new ResponseAdapter<>();
         try {
-            restTemplateCollector.setResponseEntity(this.restTemplate.exchange(uriBuilder.toUriString(), HttpMethod.POST, httpEntity, responseT));
-            restTemplateCollector.setPassResponse(true);
+            final String uri = uriBuilder.toUriString();
+            responseAdapter.setResponse(this.restTemplate.exchange(uri, HttpMethod.POST, httpEntity, responseT));
+            responseAdapter.setPassResponse(true);
         } catch (RestClientResponseException restClientResponseException) {
-            restTemplateCollector.setPassResponse(false);
-            restTemplateCollector.setRestClientResponseException(restClientResponseException);
+            responseAdapter.setPassResponse(false);
+            responseAdapter.setResponseException(restClientResponseException);
         } catch (Exception exception) {
-            restTemplateCollector.setPassResponse(false);
-            restTemplateCollector.setGeneralException(exception);
+            responseAdapter.setPassResponse(false);
+            responseAdapter.setGeneralException(exception);
         }
-        return restTemplateCollector;
+        return responseAdapter;
     }
 
-    public ClientHttpRequestFactory clientHttpRequestFactory() {
-        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
-        factory.setConnectTimeout(5000);
-        factory.setConnectionRequestTimeout(5000);
-        factory.setReadTimeout(5000);
-        return factory;
-    }
-
-    public ClientHttpRequestFactory clientSSLHttpRequestFactory() throws
-            NoSuchAlgorithmException, KeyManagementException, KeyStoreException,
-            IOException, UnrecoverableKeyException, CertificateException {
-
+    public ClientHttpRequestFactory clientSSLHttpRequestFactory() throws NoSuchAlgorithmException,
+            KeyManagementException, KeyStoreException, IOException, UnrecoverableKeyException, CertificateException {
         KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
         keyStore.load(new FileInputStream("keystore.jks"), "secret".toCharArray());
+        SSLContext sslContextBuilder = this.sslContext(keyStore);
+        SSLConnectionSocketFactory socketFactory = new SSLConnectionSocketFactory(sslContextBuilder);
+        HttpClient httpClient = this.httpClient(socketFactory);
+        return this.httpRequestFactory(httpClient);
+    }
 
-        SSLContext sslContextBuilder = new SSLContextBuilder()
+    private SSLContext sslContext(KeyStore keyStore) throws NoSuchAlgorithmException,
+            KeyStoreException, UnrecoverableKeyException, KeyManagementException {
+        return new SSLContextBuilder()
                 .loadTrustMaterial(null, new TrustSelfSignedStrategy())
                 .loadKeyMaterial(keyStore, "password".toCharArray())
                 .build();
-
-        SSLConnectionSocketFactory socketFactory = new SSLConnectionSocketFactory(sslContextBuilder);
-
-        HttpClient httpClient = HttpClients
+    }
+    private HttpClient httpClient(SSLConnectionSocketFactory socketFactory) {
+        return HttpClients
                 .custom()
                 .setSSLSocketFactory(socketFactory)
                 .build();
-
-        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(httpClient);
-        factory.setConnectTimeout(5000);
-        factory.setConnectionRequestTimeout(5000);
-        factory.setReadTimeout(5000);
-
-        return factory;
     }
+    private HttpComponentsClientHttpRequestFactory httpRequestFactory(HttpClient httpClient) {
+        HttpComponentsClientHttpRequestFactory clientHttpRequestFactory;
+
+        if (httpClient != null) {
+            clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
+        } else clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+
+        clientHttpRequestFactory.setConnectTimeout(5000);
+        clientHttpRequestFactory.setConnectionRequestTimeout(5000);
+        clientHttpRequestFactory.setReadTimeout(5000);
+        return clientHttpRequestFactory;
+    }
+
 }
--- src/test/java/org/component/rest/RestTemplateExtensionTest.java
+++ src/test/java/org/component/rest/RestTemplateExtensionTest.java
@@ -1,24 +1,33 @@
 package org.component.rest;
 
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Condition;
+import org.base.OptionalWrapper;
+import org.extensions.assertions.AssertionsExtension;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.util.UriComponentsBuilder;
-import org.utils.rest.restTemplate.RestTemplateCollector;
+import org.utils.assertions.AssertionsLevel;
+import org.utils.assertions.AssertionsManager;
+import org.utils.rest.restTemplate.ResponseAdapter;
 import org.utils.rest.restTemplate.RestTemplateExtension;
-import java.util.HashMap;
+
 import java.util.Map;
+import java.util.Optional;
+import java.util.function.Predicate;
 
 @Slf4j
+@ExtendWith(AssertionsExtension.class)
 public class RestTemplateExtensionTest {
 
     @Test
-    void restTemplateDemo() {
+    void restTemplateDemo(AssertionsManager assertionsManager) {
         RestTemplateExtension restTemplateExtension = new RestTemplateExtension();
 
         UriComponentsBuilder uriComponentsBuilder = restTemplateExtension
-                .uriBuilder("https", "jsonplaceholders.typicode.com")
+                .uriBuilder("https", "jsonplaceholder.typicode.com")
                 .path("comments")
                 .uriVariables(Map.of("postId","2"));
 
@@ -26,17 +35,20 @@ public class RestTemplateExtensionTest {
         headers.add("Content-Type","application/json");
         HttpEntity<?> httpEntity = new HttpEntity<>(headers);
 
-        RestTemplateCollector<String> restTemplateCollector = restTemplateExtension.get(uriComponentsBuilder, httpEntity, String.class);
+        ResponseAdapter<String> responseAdapter = restTemplateExtension.get(uriComponentsBuilder, httpEntity, String.class);
+        assertionsManager.setAssertionLevel(AssertionsLevel.HARD_AFTER_TEST);
 
-        if (restTemplateCollector.isPassResponse()) {
-            log.info(restTemplateCollector.getResponseEntity().getBody());
+        assertionsManager.assertThat(responseAdapter).is(new Condition<>(ResponseAdapter::isPassResponse,"response fail"));
+
+        if (responseAdapter.isPassResponse()) {
+            log.info(responseAdapter.getResponse().getBody());
         } else {
-            if (restTemplateCollector.getRestClientResponseException() != null) {
-                log.error(restTemplateCollector.getRestClientResponseException().getMessage());
-                log.error(restTemplateCollector.getRestClientResponseException().getStatusText());
-                log.error(restTemplateCollector.getRestClientResponseException().getResponseBodyAsString());
+            if (responseAdapter.getResponseException() != null) {
+                log.error(responseAdapter.getResponseException().getMessage());
+                log.error(responseAdapter.getResponseException().getStatusText());
+                log.error(responseAdapter.getResponseException().getResponseBodyAsString());
             } else {
-                log.error(restTemplateCollector.getGeneralException().getMessage());
+                log.error(responseAdapter.getGeneralException().getMessage());
             }
         }
     }
